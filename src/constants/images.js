bf2c84ed1f49eda8923ece2015215766bcdd8351
Jihaddd <jahadbamnget@gmail.com>
2024-12-09 09:36:26 +0700

    birthdaydecca

---
+++ src/constants/images.js
@@ -0,0 +1,51 @@
+import Img1 from "../assets/pics/yelsicantik1.jpg";
+import Img2 from "../assets/pics/yelsicantik5.jpg";
+import Img3 from "../assets/pics/yelsicantik6.jpg";
+import Img4 from "../assets/pics/yelsicantik7.jpg";
+import Img5 from "../assets/pics/yelsicantik2.jpg";
+import Img6 from "../assets/pics/yelsicantik8.jpg";
+import Img7 from "../assets/pics/yelsicantik4.jpg";
+import Img8 from "../assets/pics/yelsicantik3.jpg";
+import Img9 from "../assets/pics/yelsicantik9.jpg";
+import Img10 from "../assets/pics/yelsicantik10.jpg";
+import Img11 from "../assets/pics/yelsicantik11.jpg";
+import Img12 from "../assets/pics/yelsicantik12.jpg";
+
+export const images = [
+  {
+    src: Img1,
+  },
+  {
+    src: Img2,
+  },
+  {
+    src: Img3,
+  },
+  {
+    src: Img4,
+  },
+  {
+    src: Img5,
+  },
+  {
+    src: Img6,
+  },
+  {
+    src: Img7,
+  },
+  {
+    src: Img8,
+  },
+  {
+    src: Img9,
+  },
+  {
+    src: Img10,
+  },
+  {
+    src: Img11,
+  },
+  {
+    src: Img12,
+  },
+];
